1becb8ee5c223018c7905c649915b287fdb23577
beloveyeboah <beloveyeboah@gmail.com>
2023-11-24 17:10:44 -0800

    upd

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creates a puppet @ /tmp
-file {'/tmp/school':
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+# Creates a puppet @ /tmp directory
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
