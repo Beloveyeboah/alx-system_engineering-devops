ba5152b2b43b4682d360e126cfef9c3a134cfe58
beloveyeboah <beloveyeboah@gmail.com>
2024-03-22 15:43:41 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,15 @@
 # installs flask using puppet pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
 }
 
 package {'werkzeug':
-  ensure => '2.1.0',
+  ensure => '2.1.1',
   provider => 'pip3'
 }
