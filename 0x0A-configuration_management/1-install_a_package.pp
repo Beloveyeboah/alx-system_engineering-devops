8662e64031ae75a8c87d931569a2eaeceb609756
beloveyeboah <beloveyeboah@gmail.com>
2024-03-22 16:46:25 -0700

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-# this install flask with version 2.1.0
+# this install flask with version 2.1.0 on the server
 
 package { 'flask':
   ensure => '2.1.0',
   provider => 'pip3',
 }
 
-package {'werkzeug':
+package { 'werkzeug':
   ensure => '2.1.1',
-  provider => 'pip3'
+  provider => 'pip3',
 }
